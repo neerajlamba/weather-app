467a2792e4f58aed959201764ec0c00e24652133
Neeraj Lamba <neerajlamba.del@gmail.com>
2019-02-16 14:44:15 -0500

    Media Queries

--- src/App.js
+++ src/App.js
@@ -1,77 +1,85 @@
-import React, { Component } from 'react';
-import './App.css';
-import Weather from './components/Weather';
-import Form from './components/Form';
-import Titles from './components/Titles';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 
-const API_KEY = "7bc3838037ebc75e0ed597e8735542e6";
+const API_KEY = '7bc3838037ebc75e0ed597e8735542e6';
 
-class App extends Component {
-  state = {
-    temperature : undefined,
-    city : undefined,
-    country : undefined,
-    humidity : undefined,
-    description : undefined,
-    error : undefined,
-    country : undefined,
-  }
-  getWeather = async(e) => {
-    e.preventDefault();
-    console.log(e.target.elements.city.value);
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`)
-    const data = await api_call.json();
-    console.log(data.weather[0].description);
-    if (city && country) {
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
-    } else {
-      this.setState({
+
+class App extends React.Component {
+    state = {
         temperature: undefined,
         city: undefined,
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: "Please enter the values."
-      });
+        error: undefined
     }
-  }
-  render() {
-    return (
-      <div className="App">
+    getWeather = async (e) => {
+        e.preventDefault();
+        let city = e.target.elements.city.value
+
+        let country = e.target.elements.country.value;
+
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=imperial`)
+        //Convert the api data to Json format to a readable format
+        const data = await api_call.json();
+        // If the error code is 404, set State to error message else if the city and country exists run the code 
+    
+        if (data.cod === '404') {
+            this.setState({
+                error: 'Invalid Entry (Not Found)'
+            })
+        } else if (city && country) {
+
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                icon: data.weather[0].icon,
+                error: null
+            })
+        }
+    }
+
+
+    render() {
+        return (
+             <div>
         <div className="wrapper">
-          <div className="main">
+          <div className="main-fluid">
             <div className="container">
               <div className="row">
                 <div className="col-xs-5 title-container">
                   <Titles />
                 </div>
                 <div className="col-xs-7 form-container">
-                  <Form getWeather = {this.getWeather}/>
+                  <Form getWeather={this.getWeather} />
                   <Weather 
-                    temperature = {this.state.temperature}
-                    humidity = {this.state.humidity}
-                    city = {this.state.city}
-                    country = {this.state.country}
-                    description = {this.state.description}
-                    error = {this.state.error} />
+                    temperature={this.state.temperature} 
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    icon={this.state.icon}
+                    error={this.state.error}
+                  />
                 </div>
-                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+           
+        );
+    }
+};
+
+
+
+
 
 export default App;
